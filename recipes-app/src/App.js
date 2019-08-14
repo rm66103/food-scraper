2fc3d95b3ccc44d5c596253f0056f81b5d6b2b0e
Maverick <me@maverickmoore.com>
2019-08-14 15:43:43 -0400

    return random recipe from list

--- recipes-app/src/App.js
+++ recipes-app/src/App.js
@@ -2,8 +2,67 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
-class App extends React.Component {
+class RecipeCard extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return(
+      <Card style={{ width: '18rem' }}>
+        <Card.Body>
+         <a href={this.props.link}>{this.props.recipe}</a>
+        </Card.Body>
+      </Card>
+
+
+    )
+  }
+}
+
+class RandomRecipe extends React.Component{
+ constructor(props){
+   super(props);
+   this.state = {
+     'recipe': '',
+     'link': '',
+   };
+ }
+
+ componentDidMount() {
+    fetch("http://127.0.0.1:8888/recipes-from-csv")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          var random_recipe = result[Math.floor(Math.random() * result.length)]
+          this.setState({
+            recipe: random_recipe[0],
+            link: random_recipe[1],
+          });
+          console.log(this.state.recipes)
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          this.setState({
+            error
+          });
+        }
+      )
+  }
+
+ render() {
+   return(
+     <RecipeCard recipe={this.state['recipe']} link={this.state['link']}></RecipeCard>
+   )
+ }
+}
+
+class List extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -11,7 +70,7 @@ class App extends React.Component {
       isLoaded: false,
       recipes: []
     };
-  };
+  }
 
   componentDidMount() {
     fetch("http://127.0.0.1:8888/recipes-from-csv")
@@ -39,8 +98,16 @@ class App extends React.Component {
   render() {
     return(
       this.state.recipes.map(recipe =>
-        <li><a href={recipe[1]}>{recipe[0]}</a></li>
+        <RecipeCard recipe={recipe[0]} link={recipe[1]}></RecipeCard>
       )
+    )
+  }
+}
+
+class App extends React.Component{
+  render(){
+    return(
+      <RandomRecipe></RandomRecipe>
     )
   }
 }
